335fad018b465e30945cf0ac30500b8c46097612
Ariful Islam <arifulkhadija22@gmail.com>
2026-06-28 20:13:21 +0600

    update the code

---
+++ src/Components/Firebase/Firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6oG3x0kCKo9MGDXFRaC7LqJb4i64GPzY",
+  authDomain: "final-mill-manegment.firebaseapp.com",
+  projectId: "final-mill-manegment",
+  storageBucket: "final-mill-manegment.firebasestorage.app",
+  messagingSenderId: "350028222187",
+  appId: "1:350028222187:web:47e9503acc7e36566278ae"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
